2a3c20aaa02eed486af7786f7a4b6911b4a386b0
Emrullah AKTAŞ <111842183+AKTASEmrullah@users.noreply.github.com>
2022-12-26 11:18:26 +0300

    Foreach Kullanımı

---
+++ Foreach_Usage.php
@@ -0,0 +1,8 @@
+<?php
+    $Week = array("Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar");
+
+    foreach($Week as $i => $Value)
+    {
+        echo $Week[$i] . "<br>";
+    }
+?>
